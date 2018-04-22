fc1aa668ccb89fcb1348841a496e2a1e3aeca580
Brandon Trabucco <brandon@btrabucco.com>
2018-04-21 18:50:38 -0700

    added DQN to inference function (without gradient)

--- neural_heap/graph/tf_graph.py
+++ neural_heap/graph/tf_graph.py
@@ -27,7 +27,8 @@ class TFGraph(object):
                     args.LSTM_SIZE), 
                 input_keep_prob=1.0, 
                 output_keep_prob=args.DROPOUT_PROBABILITY) for i in range(args.LSTM_DEPTH)]
-            lstm_forward = tf.contrib.rnn.MultiRNNCell(lstm_forward)
+            lstm_forward = tf.contrib.rnn.MultiRNNCell(
+                lstm_forward)
             state_batch = lstm_forward.zero_state(
                 args.BATCH_SIZE,
                 tf.float32)
@@ -39,12 +40,12 @@ class TFGraph(object):
                 (scope.name + args.PREFIX_OUTPUT + args.EXTENSION_BIASES), 
                 [args.DATASET_RANGE])
 
-            operator_w = self.tf_graph_utils.initialize_weights_cpu(
+            q_function_w = self.tf_graph_utils.initialize_weights_cpu(
                 (scope.name + args.PREFIX_OPERATOR + args.EXTENSION_WEIGHTS), 
-                [args.LSTM_SIZE, args.OP_SIZE])
-            operator_b = self.tf_graph_utils.initialize_biases_cpu(
+                [args.LSTM_SIZE])
+            q_function_b = self.tf_graph_utils.initialize_biases_cpu(
                 (scope.name + args.PREFIX_OPERATOR + args.EXTENSION_BIASES), 
-                [args.OP_SIZE])
+                [1])
 
             operand_w = self.tf_graph_utils.initialize_weights_cpu(
                 (scope.name + args.PREFIX_OPERAND + args.EXTENSION_WEIGHTS), 
@@ -63,30 +64,78 @@ class TFGraph(object):
                     args.DATASET_RANGE]),
                     [args.BATCH_SIZE, args.DATASET_RANGE])
                 for i in range(args.DATASET_COLUMNS * 2)]
+            actions_batch = [
+                tf.tile(
+                    tf.reshape(
+                        tf.one_hot(i, args.OP_SIZE),
+                        [1, args.OP_SIZE]),
+                    [args.BATCH_SIZE, 1]) for i in range(args.OP_SIZE)]
             outputs_batch = []
+
 
             for i in inputs_batch:
                 x_inputs = tf.concat([
                     i, 
-                    tf.cast(heap_batch, tf.float32)], 1)
-                hidden_batch, state_batch = lstm_forward.call(
-                    x_inputs,
-                    state_batch)
+                    tf.cast(heap_batch, tf.float32)], axis=1)
+                hidden_buffer = []
+                state_buffer = []
+                q_buffer = []
+
+                print(state_batch)
+
+                for a in actions_batch:
+                    a_inputs = tf.concat([
+                        x_inputs,
+                        a], axis=1)
+                    q_hidden_batch, q_state_batch = lstm_forward.call(
+                        a_inputs,
+                        state_batch)
+                    hidden_buffer += [q_hidden_batch]
+                    state_buffer += [q_state_batch]
+                    q_buffer += [tf.add(tf.tensordot(
+                        q_hidden_batch,
+                        q_function_w,
+                        1), q_function_b)]
+
+                best_q_indices = tf.argmax(
+                    tf.stack(q_buffer, axis=1), 
+                    axis=1, 
+                    output_type=tf.int32)
+                best_q_enumerated = tf.stack([
+                        tf.range(args.BATCH_SIZE, dtype=tf.int32),
+                        best_q_indices], axis=1)
+                hidden_batch = tf.gather_nd(
+                    tf.stack(hidden_buffer, axis=1),
+                    best_q_enumerated)
+
+                state_batch = []
+                for z in range(args.LSTM_DEPTH):
+                    cell_state = tf.stack([
+                        state_buffer[a][z].c for a in range(args.OP_SIZE)
+                    ], axis=1)
+                    cell_state = tf.gather_nd(cell_state, best_q_enumerated)
+                    hidden_state = tf.stack([
+                        state_buffer[a][z].h for a in range(args.OP_SIZE)
+                    ], axis=1)
+                    hidden_state = tf.gather_nd(hidden_state, best_q_enumerated)
+                    lstm_tuple = tf.contrib.rnn.LSTMStateTuple(
+                        cell_state, 
+                        hidden_state)
+                    state_batch += [lstm_tuple]
+                state_batch = tuple(state_batch)
+                print(state_batch)
+
                 outputs_batch += [tf.add(tf.tensordot(
                     hidden_batch,
                     controller_w,
                     1), controller_b)]
-                operator_batch = tf.add(tf.tensordot(
-                    hidden_batch,
-                    operator_w,
-                    1), operator_b)
                 operand_batch = tf.add(tf.tensordot(
                     hidden_batch,
                     operand_w,
                     1), operand_b)
                 heap_batch = self.heap.interact(
                     args,
-                    operator_batch,
+                    tf.one_hot(best_q_indices, 3),
                     operand_batch)
             
             parameters = tf.get_collection(
